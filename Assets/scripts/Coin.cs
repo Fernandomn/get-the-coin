48973444dfc0e40a8261eb83637caa1762c45a6a
Fernando Medeiros do Nascimento <fernando_m_n@hotmail.com>
2020-02-28 19:07:29 -0300

    First Commit
    First commit and first Github on Unity use

---
+++ Assets/scripts/Coin.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    public static int CoinCount = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ++Coin.CoinCount;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        //Debug.Log("Colidiu");
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("BOOOOM!!!");
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        --Coin.CoinCount;
+
+        if (Coin.CoinCount <= 0)
+        {
+            GameObject Timer = GameObject.Find("LevelTimer");
+            Destroy(Timer);
+
+            GameObject[] FireworksSystems = GameObject.FindGameObjectsWithTag("Fireworks");
+            foreach (GameObject firework in FireworksSystems)
+            {
+                firework.GetComponent<ParticleSystem>().Play();
+            }
+        }
+    }
+
+}
